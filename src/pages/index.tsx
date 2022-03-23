46686a5e702769414f1bc60952b9d1a70eda1ab9
Lukas Woodtli <woodtli.lukas@gmail.com>
2022-03-23 09:29:15 +0100

    Add date formatting example
    Change-Id: I2f2a2efefc98f32807c815d57c68d92364fdf137

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import * as React from "react"
 import Layout from "../components/layout";
+import moment from "moment";
 
 const IndexPage = () => {
   return (
     <Layout>
       <h1>My landing Pager</h1>
+        <div>Date: {moment(new Date(9999234569999)).format('D MMMM YYYY')}</div>
     </Layout>
   )
 }
