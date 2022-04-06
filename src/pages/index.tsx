522d915333ac9004dd76c11c548c4acfe514ae0f
Lukas Woodtli <woodtli.lukas@gmail.com>
2022-04-06 08:45:33 +0200

    Experimenting with layout
    Change-Id: Ic4699ab6055e7cc9c47a72fea08c07b933770912

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,41 @@
 import * as React from "react"
 import Layout from "../components/layout";
 import moment from "moment";
+import {Grid, Paper, styled} from "@mui/material";
+
+const Item = styled(Paper)(({ theme }) => ({
+    spacing: 0,
+    square: true,
+    padding: 0,
+    textAlign: 'left',
+    color: theme.palette.text.secondary,
+    elevation: 0
+}));
 
 const IndexPage = () => {
   return (
     <Layout>
-      <h1>My landing Page</h1>
-        <div>Date: {moment(new Date(9999234569999)).format('D MMMM YYYY')}</div>
+
+        <Grid container
+              spacing={0}
+              direction="row"
+              justifyContent="center"
+              alignItems="stretch"
+              columns={{ xs: 3, sm: 3, md: 1, lg: 1 }}>
+            <Grid item sx={{xs: 2}}>
+                <Item>xs=8</Item>
+            </Grid>
+            <Grid item xs={4}>
+                <Item>
+                    <h1>My landing Page</h1>
+                    <div>Date: {moment(new Date(9999234569999)).format('D MMMM YYYY')}</div>
+                </Item>
+            </Grid>
+            <Grid item xs={4}>
+                <Item>Hello</Item>
+            </Grid>
+        </Grid>
+
     </Layout>
   )
 }
